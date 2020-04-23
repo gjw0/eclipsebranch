de64a19037859dc69f7072a87786b6ffc6464a1a
gjw0 <1668447489@qq.com>
2020-04-24 01:21:22 +0800

    zzzzzdev2

--- gitbranch/src/main/java/com/kk/controller/MyController2.java
+++ gitbranch/src/main/java/com/kk/controller/MyController2.java
@@ -1,15 +1,23 @@
 package com.kk.controller;
 
+
+
+import java.util.Date;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 @Controller
 public class MyController2 {
 	
+	Date date = new java.util.Date();
+	String SseEmitter = "sdfasdfs";
 	@RequestMapping("/a")
 	@ResponseBody
 	public String mm() {
+		Integer aa = Integer.valueOf(34);
 		return "aaa";
 	}
 	Integer a = Integer.valueOf(3);
---
+++ gitbranch/src/main/java/com/kk/controller/MyController3.java
@@ -0,0 +1,18 @@
+package com.kk.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController3 {
+	
+	private String name ;
+	private Integer num;
+	
+	public void say() {
+		System.out.println("dfasdfasdf");
+		Integer a = Integer.valueOf(4);
+		
+	}
+	
+	
+}
