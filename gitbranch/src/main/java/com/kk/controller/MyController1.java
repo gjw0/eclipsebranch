2333c4c2c674f86b24bc9a70581b2ed4c82b8526
gjw0 <1668447489@qq.com>
2020-04-24 00:29:39 +0800

    addcon1dev1

---
+++ gitbranch/src/main/java/com/kk/controller/MyController1.java
@@ -0,0 +1,17 @@
+package com.kk.controller;
+
+import java.sql.Date;
+
+public class MyController1 {
+	
+	String aString = "sdfas";
+	public void say() {
+		System.out.println("asdfasdfasd");
+	}
+	Date date = new Date(3434L);
+	Integer asd = Integer.valueOf(435);
+	
+	
+	
+	
+}
